156a2d7734d26f94775844bde34ae6739a4a05d4
Valen Cheung <xvalen214x@gmail.com>
2019-05-15 19:15:25 +0800

    ssh changes

---
+++ data/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_MmEZwEYkQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '20294b2456gtqnhg7m16u3sls7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
